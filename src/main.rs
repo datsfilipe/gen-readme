237796dfe55667ea4d26ebb15d9a626072fbee48
datsfilipe <datsfilipe.foss@proton.me>
2023-07-21 16:57:41 -0300

    feat: copy template as readme in the current dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::env;
+
+fn main() -> std::io::Result<()> {
+    let current_dir = env::current_dir().unwrap();
+    let readme_path = current_dir.join("README.md");
+    let readme_template = include_str!("../readme-template.md");
+    let readme = readme_template;
+    fs::write(readme_path, readme).unwrap();
+    Ok(())
 }
