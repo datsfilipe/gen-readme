38a5c11585efcf8f427d049faa6cb0dc840ed838
datsfilipe <datsfilipe.foss@proton.me>
2023-08-29 15:10:20 -0300

    feat: add selector function to select entries

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,15 @@ struct Asset {
     path: String,
     name: String,
 }
+fn selector(options: &Vec<&String>) -> Vec<usize> {
+    let selection = MultiSelect::with_theme(&ColorfulTheme::default())
+        .items(options)
+        .interact_on_opt(&Term::stderr())
+        .unwrap();
+
+    match selection {
+        Some(selection) => return selection,
+        None => return vec![],
+    }
+}
+
